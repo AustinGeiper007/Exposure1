106e49e03885b2bf6776c544ce79164287799959
Austin G <austin.geisler@vtsd.com>
2025-06-03 10:33:09 -0400

    Spelling correction

--- Lab03/AGLab03.java
+++ Lab03/AGLab03.java
@@ -7,13 +7,13 @@ public class AGLab03 {
         int hours;
         int minutes;
         int seconds;
-        int miliseconds;
+        int milliseconds;
 
         // Do the math (they did the monster math)
         hours = startingMilliseconds / 3600000;
         minutes = (startingMilliseconds - hours * 3600000) / 60000;
         seconds = (startingMilliseconds - hours * 3600000 - minutes * 60000) / 1000;
-        miliseconds = (startingMilliseconds - hours * 3600000 - minutes * 60000 - seconds * 1000);
+        milliseconds = (startingMilliseconds - hours * 3600000 - minutes * 60000 - seconds * 1000);
 
         // Print it so you can actually see what happened!
         System.out.println("Lab03: 100 pt version \n");
@@ -21,7 +21,7 @@ public class AGLab03 {
         System.out.println("Hours: \t\t\t\t\t" + hours);
         System.out.println("Minutes: \t\t\t\t" + minutes);
         System.out.println("Seconds: \t\t\t\t" + seconds);
-        System.out.println("Milliseconds: \t\t\t" + miliseconds);
+        System.out.println("Milliseconds: \t\t\t" + milliseconds);
 
     }
 }
